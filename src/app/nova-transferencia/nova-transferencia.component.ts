0411003e02d63e9f7a834ef2079c2e029eb651d5
JandeAJR <jande.ajr@gmail.com>
2022-04-23 20:06:37 -0300

    commit de conclusão do projeto

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,6 +1,7 @@
 import { Component, Output, EventEmitter } from "@angular/core";
 import { TransferenciaService } from '../services/transferencia.service';
 import { Transferencia } from '../models/transferencia.model';
+import { Router } from "@angular/router";
 
 @Component ({
   selector:'app-nova-transferencia',
@@ -13,26 +14,30 @@ export class NovaTransferenciaComponent {
 
   valor: number = 0;
   destino: number = 0;
-  descricao: string = "";
+  descricao: string = '';
 
-  constructor(private service: TransferenciaService) {}
+  constructor(private service: TransferenciaService,
+    private router: Router) {}
 
   transferir() {
-    console.log("Solicitada nova transferência");
+    console.log('Solicitada nova transferência');
 
     //const valorEmitir = {valor: this.valor, destino: this.destino, descricao: this.descricao};
     const valorEmitir: Transferencia = {valor: this.valor, destino: this.destino, descricao: this.descricao};
 
+    // Forma nova de implementar o método subscribe (versões novas do Angular)
     this.service.adicionar(valorEmitir).subscribe({
-      next: () => this.limparCampos(),
+      next: () => [this.limparCampos(), this.router.navigateByUrl('extrato')],
       error: (error) => console.error(error),
       complete: () => console.info('complete!')
     })
 
+    // Forma antiga de implementar o método subscribe (versões anteriores do Angular)
     /*
     this.service.adicionar(valorEmitir).subscribe((resultado) => {
         console.log(resultado);
         this.limparCampos();
+        this.router.navigateByUrl('extrato');
       },
       (error) => console.error(error)
     );
@@ -45,7 +50,7 @@ export class NovaTransferenciaComponent {
   limparCampos() {
     this.valor = 0;
     this.destino = 0;
-    this.descricao = "";
+    this.descricao = '';
   }
 
 }
